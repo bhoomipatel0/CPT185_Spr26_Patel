1733e9b53920325343fd48c9866f8ffb4ca6fc95
Bhoomi Patel <patel.bhoomi218@gmail.com>
2026-04-19 15:20:20 -0400

    Add files via upload

---
+++ VB9e/Chap11/Databases/Cookies Project/Cookies Project/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CookiesDBDataSet = New Cookies_Project.CookiesDBDataSet()
+        Me.SalesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.SalesTableAdapter = New Cookies_Project.CookiesDBDataSetTableAdapters.SalesTableAdapter()
+        Me.TableAdapterManager = New Cookies_Project.CookiesDBDataSetTableAdapters.TableAdapterManager()
+        Me.SalesBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.SalesBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.SalesDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.CookiesDBDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SalesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SalesBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SalesBindingNavigator.SuspendLayout()
+        CType(Me.SalesDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CookiesDBDataSet
+        '
+        Me.CookiesDBDataSet.DataSetName = "CookiesDBDataSet"
+        Me.CookiesDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'SalesBindingSource
+        '
+        Me.SalesBindingSource.DataMember = "Sales"
+        Me.SalesBindingSource.DataSource = Me.CookiesDBDataSet
+        '
+        'SalesTableAdapter
+        '
+        Me.SalesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.SalesTableAdapter = Me.SalesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Cookies_Project.CookiesDBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'SalesBindingNavigator
+        '
+        Me.SalesBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.SalesBindingNavigator.BindingSource = Me.SalesBindingSource
+        Me.SalesBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.SalesBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.SalesBindingNavigator.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.SalesBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.SalesBindingNavigatorSaveItem})
+        Me.SalesBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.SalesBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.SalesBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.SalesBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.SalesBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.SalesBindingNavigator.Name = "SalesBindingNavigator"
+        Me.SalesBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.SalesBindingNavigator.Size = New System.Drawing.Size(1374, 42)
+        Me.SalesBindingNavigator.TabIndex = 0
+        Me.SalesBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(46, 19)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(46, 36)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 39)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(70, 32)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(46, 36)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(46, 36)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(46, 36)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(46, 36)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'SalesBindingNavigatorSaveItem
+        '
+        Me.SalesBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.SalesBindingNavigatorSaveItem.Image = CType(resources.GetObject("SalesBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.SalesBindingNavigatorSaveItem.Name = "SalesBindingNavigatorSaveItem"
+        Me.SalesBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.SalesBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'SalesDataGridView
+        '
+        Me.SalesDataGridView.AutoGenerateColumns = False
+        Me.SalesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.SalesDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
+        Me.SalesDataGridView.DataSource = Me.SalesBindingSource
+        Me.SalesDataGridView.Location = New System.Drawing.Point(28, 155)
+        Me.SalesDataGridView.Name = "SalesDataGridView"
+        Me.SalesDataGridView.RowHeadersWidth = 82
+        Me.SalesDataGridView.RowTemplate.Height = 33
+        Me.SalesDataGridView.Size = New System.Drawing.Size(1243, 481)
+        Me.SalesDataGridView.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Week"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Week"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 10
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        Me.DataGridViewTextBoxColumn1.Width = 200
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "ChocolateChip"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "ChocolateChip"
+        Me.DataGridViewTextBoxColumn2.MinimumWidth = 10
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.Width = 200
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "PeanutButter"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "PeanutButter"
+        Me.DataGridViewTextBoxColumn3.MinimumWidth = 10
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.Width = 200
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "PecanSandies"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "PecanSandies"
+        Me.DataGridViewTextBoxColumn4.MinimumWidth = 10
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        Me.DataGridViewTextBoxColumn4.Width = 200
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1374, 688)
+        Me.Controls.Add(Me.SalesDataGridView)
+        Me.Controls.Add(Me.SalesBindingNavigator)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.CookiesDBDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SalesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SalesBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SalesBindingNavigator.ResumeLayout(False)
+        Me.SalesBindingNavigator.PerformLayout()
+        CType(Me.SalesDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CookiesDBDataSet As CookiesDBDataSet
+    Friend WithEvents SalesBindingSource As BindingSource
+    Friend WithEvents SalesTableAdapter As CookiesDBDataSetTableAdapters.SalesTableAdapter
+    Friend WithEvents TableAdapterManager As CookiesDBDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents SalesBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents SalesBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents SalesDataGridView As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+End Class
---
+++ VB9e/Chap11/Databases/Cookies Project/Cookies Project/Form1.vb
@@ -0,0 +1,26 @@
+﻿'Project11: create an application that keeps track of cookie sales.
+'Name : Bhoomi Patel
+'Subject : Event Driven Programming
+'Date : 11 April 2026
+
+
+Public Class Form1
+    Private Sub SalesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles SalesBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.SalesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.CookiesDBDataSet)
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CookiesDBDataSet.Sales' table. You can move, or remove it, as needed.
+        Me.SalesTableAdapter.Fill(Me.CookiesDBDataSet.Sales)
+
+    End Sub
+
+    Private Sub SalesDataGridView_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles SalesDataGridView.CellContentClick
+
+    End Sub
+
+End Class
